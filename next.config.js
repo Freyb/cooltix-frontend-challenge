2e14dea531fedaf29a3ee625904a94554eee6f34
Freyb <balazs.frey@gmail.com>
2023-10-02 14:19:34 +0200

    Enable css prop for styled-components

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    styledComponents: {
+      ssr: true,
+    },
+  },
   images: {
     domains: ['randomuser.me'],
   },

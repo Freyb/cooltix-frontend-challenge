3de10060d1c9635246e66bb3594e1a949bfa0254
Freyb <balazs.frey@gmail.com>
2023-10-04 16:06:13 +0200

    Add loading page

---
+++ src/app/member/[id]/loading.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+
+export default function Loading() {
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Image src="/logo_square_animated.svg" height={200} width={200} alt="logo_square" />
+    </div>
+  );
+}
